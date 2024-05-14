2036b77ba5770a78a826cf4bf234b3341e61004c
John Lemar Gonzales <138680080+CyberSphinxxx@users.noreply.github.com>
2024-05-14 19:04:03 +0800

    Update RoomBookingSys.py

--- RoomBookingSys.py
+++ RoomBookingSys.py
@@ -1,19 +1,20 @@
 def book_room(room_list):
     if not room_list:
-        print("All rooms have been booked.")
+        print("Sorry, all rooms have been booked!")
         return
 
     print("Available rooms:", room_list)
-    room = input("Enter the room you want to book (e.g., 'Room 101'): ")
+    room = input("Enter the room you want to book (example, 'Room 1'): ")
 
     if room not in room_list:
-        print("Invalid room. Please choose from the available rooms.")
+        print("Invalid room. Please choose from the available rooms in the list.")
         book_room(room_list)
         return
 
     client_name = input("Enter client's name: ")
     payment = input("Enter payment amount: ")
 
+    print(line)
     print("Booking confirmed for " + client_name + " in " + room + " with payment of " + payment + ".")
 
     room_list.remove(room)
@@ -22,7 +23,13 @@ def book_room(room_list):
     if another_booking.lower() == 'yes':
         book_room(room_list)
     else:
+        print(line)
         print("Thank you for your bookings!")
+        print("Enjoy your stay!")
 
-available_rooms = ['Room 101', 'Room 102', 'Room 103', 'Room 104']
+line = "--------------------------------------------------------------------"
+
+print(line)
+available_rooms = ['Room 1', 'Room 2', 'Room 3', 'Room 4', 'Room 5']
 book_room(available_rooms)
+print(line)
